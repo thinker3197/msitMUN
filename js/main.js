4cf2bf1d67dcc92613a6ee3d1c12b2e2453b34e7
Ashish <ashishgupta.3197@gmail.com>
2016-03-17 21:32:58 +0530

    Add files

---
+++ js/main.js
@@ -0,0 +1,23 @@
+jQuery(document).ready(function($) {
+    var isLateralNavAnimating = false;
+
+    $('.cd-nav-trigger').on('click', function(event) {
+        event.preventDefault();
+
+        if (!isLateralNavAnimating) {
+            if ($(this).parents('.csstransitions').length > 0) isLateralNavAnimating = true;
+
+            $('body').toggleClass('navigation-is-open');
+            $('.cd-navigation-wrapper').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function() {
+
+                isLateralNavAnimating = false;
+            });
+        }
+    });
+    $(function() {
+        if (navigator.userAgent.indexOf('Safari') != -1 &&
+            navigator.userAgent.indexOf('Chrome') == -1) {
+            $("body").addClass("safari");
+        }
+    });
+});
