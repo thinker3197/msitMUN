2a1238bd6e56811eeea6af8fa365cd0ecbc78c27
Ashish <ashishgupta.3197@gmail.com>
2016-03-18 09:59:55 +0530

    Minor Changes

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 jQuery(document).ready(function($) {
     var isLateralNavAnimating = false;
 
-    $('.cd-nav-trigger').on('click', function(event) {
+    $('.nav-trigger').on('click', function(event) {
         event.preventDefault();
 
         if (!isLateralNavAnimating) {
             if ($(this).parents('.csstransitions').length > 0) isLateralNavAnimating = true;
 
             $('body').toggleClass('navigation-is-open');
-            $('.cd-navigation-wrapper').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function() {
+            $('.navigation-wrapper').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function() {
 
                 isLateralNavAnimating = false;
             });
